c60611daf70f36e8217914304734bb56c3555a5c
cfuke1 <156386553+cfuke1@users.noreply.github.com>
2024-06-08 23:35:05 -1000

    commit
    commit

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
 
-class Test
-{
-    public static void main(String []args)
-    {
-        System.out.println("My First Java Program.");
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList sayings = new LinkedList();
+
+        Node first = new Node("E kaupē aku nō i ka hoe a kō mai.", "Put forward the paddle and draw it back.", "Go on with the task that is started and finish it", "Hawaiian Explanation");
+
+        sayings.add(first);
+
+        System.out.println("Sorted List: " + sayings);
     }
-};
+}
--- Node.java
+++ Node.java
@@ -7,13 +7,15 @@ public class Node {
     Node right;
     Node parent;
 
-    boolean color;
+    boolean color; //Red is FALSE, Black is TRUE
 
     public Node(String foreignSaying, String englishSaying, String foreignExplanation, String englishExplanation) {
         this.foreignSaying = foreignSaying;
         this.englishSaying = englishSaying;
         this.foreignExplanation = foreignExplanation;
         this.englishExplanation = englishExplanation;
+
+
     }
 
 }
