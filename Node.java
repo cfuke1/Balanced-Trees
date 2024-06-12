e88d575568e676cb7bd377df2a310e22377af118
cfuke1 <156386553+cfuke1@users.noreply.github.com>
2024-06-11 19:59:33 -1000

    commit
    commit

--- Node.java
+++ Node.java
@@ -15,38 +15,4 @@ class Node {
         this.englishExplanation = englishExplanation;
         this.height = 1;
     }
-
-    public static String hawaiianToEnglish(String hawaiianWord) {
-        StringBuilder englishWord = new StringBuilder();
-
-        for (char c : hawaiianWord.toCharArray()) {
-            char englishEquivalent = getEnglishEquivalent(c);
-            englishWord.append(englishEquivalent);
-        }
-        return englishWord.toString();
-    }
-
-    // Function to get the English equivalent of a Hawaiian letter
-    private static char getEnglishEquivalent(char hawaiianLetter) {
-        switch (hawaiianLetter) {
-            case 'Ā':
-            case 'ā':
-                return 'a';
-            case 'Ē':
-            case 'ē':
-                return 'e';
-            case 'Ī':
-            case 'ī':
-                return 'i';
-            case 'Ō':
-            case 'ō':
-                return 'o';
-            case 'Ū':
-            case 'ū':
-                return 'u';
-            default:
-                return hawaiianLetter;
-        }
-    }
-
 }
